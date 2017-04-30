f25a7b9f86f004890ab4fa24cb3c82a1c4d014d8
z64 <zachnowicki@gmail.com>
2017-04-30 00:29:20 -0400

    Entry#older_than?, Entry.prune

--- src/cr/model/entry.rb
+++ src/cr/model/entry.rb
@@ -19,5 +19,18 @@ module ChelshiaRocks
       Time.at(score[0..-4].to_f + score[-3..-1].to_f / 1000)
           .strftime("%M:%S.%L")
     end
+
+    # Alias for checking if an entry is older
+    # than a given time
+    def older_than?(time)
+      time > timestamp
+    end
+
+    # Destroys any records older than a given time
+    def self.prune!(time)
+      to_a.each do |e|
+        e.destroy if e.older_than? time
+      end
+    end
   end
 end
