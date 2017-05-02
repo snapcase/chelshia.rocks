937437709e83860d3d06b024c3b7442f3e6d0253
z64 <zachnowicki@gmail.com>
2017-05-02 15:23:18 +0000

    add a route and template for user history on a board

--- src/cr/controller/leaderboard_controller.rb
+++ src/cr/controller/leaderboard_controller.rb
@@ -8,4 +8,12 @@ class LeaderboardController < ApplicationController
     @leaderboard = ChelshiaRocks::Leaderboard.leaderboard(params['id'])
     haml :leaderboard
   end
+
+  get '/:board_id/:user_id' do
+    @leaderboard = ChelshiaRocks::Leaderboard.leaderboard(params['board_id'])
+    @user = ChelshiaRocks::User.user(params['user_id'])
+    @entries = @leaderboard.entrys.where(user: @user).to_a
+
+    haml :leaderboard_user
+  end
 end
